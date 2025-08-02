cfba7386afaf6cf2d816b2065e5aa13975908382
laxjovial <laxjovial@gmail.com>
2025-08-02 15:35:04 +0100

    Update together.py

--- together.py
+++ together.py
@@ -1,23 +1,30 @@
 from together import Together
 from dotenv import load_dotenv
 import os
-#load environment variables from .env file
-#load_dotenv()
-#secret_key =os.gotenv("api_key")
-client = Together(api_key="c56046f8febede7b8c4fd1c94e6d8183437e8ab7c5048fc59ca4bbd9a2e79f60") # auth defaults to os.environ.get()
-#define your prompt
-text = "Machine learning is a field of artificial intelligence that enables computers to learn from data and make predictions"
-prompt = f"""Explain the key concepts of the text delimmited by triple backticks in simple terms. ```{text}```"""
-#add your prompt to an endpoint
+# Load environment variables from .env file
+load_dotenv()
+secret_key = os.getenv("api_key")
+client = Together(api_key=secret_key)
+# Define your prompt
+text = "Machine learning is a field of artificial intelligence that enables computers to learn from data and make prediction."
+prompt = f"""Explain the key concepts of the text delimited by triple backticks in simple terms. ```{text}```"""
+system_prompt = f"""You should explain everything machine learning, emmerging technologies, improvements and limitations and ongoing research addressed at solving these limitations"""
+# Add your prompt to your end-point
 response = client.chat.completions.create(
-    model="Qwen/Qwen3-235B-A22B-Thinking-2507",
+    model="meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
     messages=[
       {
         "role": "user",
         "content": prompt
+      },
+      {
+        "role": "system",
+        "content": system_prompt
+      },
+      {
+        "role": "assistant",
+        "content": "You are a data science specialist"
       }
     ]
 )
 print(response.choices[0].message.content)
-#Client = Together(api_key="c56046f8febede7b8c4fd1c94e6d8183437e8ab7c5048fc59ca4bbd9a2e79f60") # auth defaults to os.environ.get()
-#print(prompt)
